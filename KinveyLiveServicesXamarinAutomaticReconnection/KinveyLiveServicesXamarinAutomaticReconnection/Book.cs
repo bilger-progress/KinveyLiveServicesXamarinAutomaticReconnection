e6ba3bc686102b1d812534bf7c0ae556f8cad7e3
bilger-progress <bilger.yahov@progress.com>
2019-01-21 16:30:26 +0200

    Implement KLS.

---
+++ KinveyLiveServicesXamarinAutomaticReconnection/KinveyLiveServicesXamarinAutomaticReconnection/Book.cs
@@ -0,0 +1,18 @@
+﻿using Kinvey;
+using Newtonsoft.Json;
+
+namespace KinveyLiveServicesXamarinAutomaticReconnection
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Book : Entity
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("genre")]
+        public string Genre { get; set; }
+
+        [JsonProperty("publish_date")]
+        public string PublishDate { get; set; }
+    }
+}
--- KinveyLiveServicesXamarinAutomaticReconnection/KinveyLiveServicesXamarinAutomaticReconnection/MainPage.xaml.cs
+++ KinveyLiveServicesXamarinAutomaticReconnection/KinveyLiveServicesXamarinAutomaticReconnection/MainPage.xaml.cs
@@ -4,14 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Kinvey;
 
 namespace KinveyLiveServicesXamarinAutomaticReconnection
 {
     public partial class MainPage : ContentPage
     {
+        private readonly Client KinveyClient;
+        private string appKey = "xxx";
+        private string appSecret = "xxx";
+        private string userName = "xxx";
+        private string userPassword = "xxx";
+
         public MainPage()
         {
             InitializeComponent();
+            Client.Builder Builder = new Client.Builder(this.appKey, this.appSecret);
+            this.KinveyClient = Builder.Build();
+            this.KinveyPing();
+            this.ProceedKinveyLiveServices();
+        }
+
+        private async void KinveyPing()
+        {
+            try
+            {
+                PingResponse PingResponse = await this.KinveyClient.PingAsync();
+                Console.WriteLine("Kinvey Ping Response: " + PingResponse.kinvey);
+            }
+            catch (Exception PingException)
+            {
+                Console.WriteLine("Kinvey Ping Exception: " + PingException.Message);
+            }
+        }
+
+        private async void ProceedKinveyLiveServices()
+        {
+            if (!Client.SharedClient.IsUserLoggedIn())
+            {
+                await User.LoginAsync(this.userName, this.userPassword);
+            }
+
+            await Client.SharedClient.ActiveUser.RegisterRealtimeAsync();
+            DataStore<Book> Books = DataStore<Book>.Collection("Books");
+            await Books.Subscribe(new KinveyDataStoreDelegate<Book>
+            {
+                OnNext = (result) => {
+                    Console.WriteLine("KLS Book title: " + result.Title);
+                },
+                OnStatus = (status) => {
+                    Console.WriteLine("KLS Subscription Status Change: " + status.Message);
+                },
+                OnError = (error) => {
+                    Console.WriteLine("KLS Error: " + error.Message);
+                }
+            });
         }
     }
 }
